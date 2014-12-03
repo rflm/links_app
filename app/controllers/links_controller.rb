b3f0a626585a2666b980b8e7025f1e62f6a54e9c
rafal <rafalmularski@outlook.com>
2014-12-03 15:15:35 +0100

    Fully working links

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,50 @@
+class LinksController < ApplicationController
+
+  def index
+    @links = Link.paginate(page: params[:page])
+  end
+
+  def show
+    @link = Link.find(params[:id])
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      flash[:success] = "Link added!"
+      redirect_to @link
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update_attributes(link_params)
+      flash[:success] = "Link updated!"
+      redirect_to @link
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Link.find(params[:id]).destroy
+    flash[:success] = "Link destroyed!"
+    redirect_to links_url
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:title, :url, :description)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+99.times do |n|
+  title = Faker::Lorem.sentence
+  url = Faker::Internet.url
+  description = Faker::Lorem.paragraph
+  Link.create!(title: title, url: url, description: description)
+end
+
