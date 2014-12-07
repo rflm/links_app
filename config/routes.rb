a2072dab644718e7ea3370b9912f5090ad54c4b2
rafal <rafalmularski@outlook.com>
2014-12-07 09:18:52 +0100

    Sessions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :links
+  root 'links#index'  
 
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
-  root 'links#index'
+  resources :users
+  resources :links
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
