45329dfb936864484c513db16f592262fa0d9424
rafal <rafalmularski@outlook.com>
2015-01-25 15:41:30 +0100

    Removing bugs in links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  before_action :logged_in_user, only: [:vote, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:new, :create, :vote, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
