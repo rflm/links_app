e712efff1edb72b7c76b648d45181c04faed0d32
rafal <rafalmularski@outlook.com>
2015-03-22 17:51:10 +0100

    _link partial fixed

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,9 @@
 class Link < ActiveRecord::Base
+  belongs_to :user
   has_many :comments
   has_reputation :votes, source: :user, aggregated_by: :sum
   
   validates :title, presence: true
   validates :url, presence: true
+  
 end
