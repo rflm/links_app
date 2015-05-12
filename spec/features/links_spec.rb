4d20ca7908eec7fe74666ed407583619a3adbb99
rflm <rafalmularski@outlook.com>
2015-05-12 16:19:25 +0200

    Removing unnecesseries controllers and specs

--- spec/features/links_spec.rb
+++ spec/features/links_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 describe 'add link process' do
 	it 'adds link' do
 		visit new_link_path
-		fill_in 'Title', with: 'Example link'
-		fill_in 'Url', with: 'http://example-link.com'
-		fill_in 'Description', with: 'Description'
+		fill_in 'link_title', with: 'Example link'
+		fill_in 'link_url', with: 'http://example-link.com'
+		fill_in 'link_description', with: 'Description'
 		click_button 'Save'
-		expect(page).to have_content 'link added'
+		expect(page).to have_content 'Link added!'
 	end
 end
