28d6fda2424f5f410a5d315b336a198a30d6f92e
rflm <rafalmularski@outlook.com>
2015-05-18 23:21:33 +0200

    Deleting sessions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+	it "should have a current_user" do
+		@user = FactoryGirl.create(:user)
+		sign_in(:user, @user)
+    subject.current_user.should_not be_nil
+  end
+
+  it "should get index" do
+    get 'index'
+    response.should be_success
+  end
 end
